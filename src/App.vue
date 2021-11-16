055297c5de176e54e54b5a54d65a55bf44312d44
niels <niels>
2021-11-16 16:31:43 +0100

    Nanko update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+	<!-- <div id="nav">
+		<router-link to="/">Home</router-link> |
+		<router-link to="/about">About</router-link>
+	</div>
+	<router-view/> -->
+	<main>
+		<h1>Vroom! Vroom!</h1>
+		<h2>Aww, stoer hoor Nanko</h2>
+		<h2>Ga je lekker spelen op je motorfietsje?</h2>
+	</main>
 </template>
 
 <style lang="scss">
+html, body {
+	padding: 0;
+	margin: 0;
+	border: 0;
+	background: black;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+//   color: #2c3e50;
+	margin: 0;
+	color: white;
+	-webkit-text-stroke-width: 1px;
+	-webkit-text-stroke-color: black;
+	background: url('./assets/pp.jpeg');
+	height: 100vh;
+	width: 100vw;
+	h1 {
+		display: inline-block;
+		padding: 3rem 0 0 0;
+		font-size: 5vw;
+	}
+	h2 {
+		font-size: 4vw;
+	}
 }
 
 #nav {
